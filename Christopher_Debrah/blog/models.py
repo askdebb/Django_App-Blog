4a26a3e33156b76f409e87bd141d24bb36d2bd26
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2022-06-18 23:43:36 +0000

    Django Blog Post done

---
+++ Christopher_Debrah/blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+class Post(models.Model):
+    Title = models.CharField(max_length=200)
+    Text = models.TextField()
+    Author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    Created_date = models.DateTimeField()
+    Published_date = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return self.Title
