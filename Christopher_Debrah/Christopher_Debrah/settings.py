5137fd820521c74a9ac1eee4275ed06c2898f1cf
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2022-06-19 02:32:21 +0000

    Final Modification to model tags for post.html

--- Christopher_Debrah/Christopher_Debrah/settings.py
+++ Christopher_Debrah/Christopher_Debrah/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+from re import T
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = 'django-insecure-$m0mu%x7v&ybhw-04j=o-0b^+uuuklt=h+i+qez*csr$($g#ip'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
--- Christopher_Debrah/blog/views.py
+++ Christopher_Debrah/blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 def home(request):
     return render(request, 'home.html', {})
 
 def post(request):
-    return render(request, 'post.html', {})
+    bps = Post.objects.all().order_by('Created_date')
+    return render(request, 'post.html', {'bp':bps})
