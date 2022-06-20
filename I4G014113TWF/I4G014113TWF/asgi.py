5052035c26a36d6714878e6ae3906bc6eff1491f
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2022-06-20 17:22:20 +0000

    Updated files Renamed with ID

--- I4G014113TWF/I4G014113TWF/asgi.py
+++ I4G014113TWF/I4G014113TWF/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Christopher_Debrah project.
+ASGI config for I4G014113TWF project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Christopher_Debrah.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'I4G014113TWF.settings')
 
 application = get_asgi_application()
--- I4G014113TWF/manage.py
+++ I4G014113TWF/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Christopher_Debrah.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'I4G014113TWF.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
